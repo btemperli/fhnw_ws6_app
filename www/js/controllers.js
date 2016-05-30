bea5d19173370b08ff62521aab4e0fbbfc3aa411
Beat Temperli <beat@gridonic.ch>
2016-05-30 17:08:39 +0200

    added first server request

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,10 @@
 angular.module('app.controllers', [])
   
-.controller('schuldenCtrl', function($scope) {
-
+.controller('schuldenCtrl', function($scope, Schulden) {
+    Schulden.getAllTo(1).success(function (response) {
+        console.log(response);
+        // $scope.chats = response;
+    })
 })
    
 .controller('statistikenCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -4,6 +4,18 @@ angular.module('app.services', [])
 
 }])
 
+.factory('Schulden', function ($http, $rootScope, $stateParams) {
+
+    return {
+        getAllTo: function (id) {
+            return $http.get('http://fhnw.temper.li/get-debts-to', { params: { id:id } })
+        },
+        pay: function (id) {
+            return $http.post('http://fhnw.temper.li/debt/pay', { params: {id:id}})
+        }
+    };
+})
+
 .service('BlankService', [function(){
 
 }]);
